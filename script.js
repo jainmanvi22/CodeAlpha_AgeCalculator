06b21ec989b03182edba614bd4d0e2a300e808b6
jainmanvi22 <jainmanvi227@gmail.com>
2024-07-04 21:08:56 +0530

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-//Define funtion to get calculated Age
 function getDOB() {
-
-    // Getting input from html input element
     let data =
         document.getElementById("inputDob").value;
-
-    // Convert input data to usable format
-    // as day,month and year
     let dob = new Date(data);
     let day = dob.getDate();
     let month = dob.getMonth();
     let year = dob.getFullYear();
-
-    // Getting current date and calculating the difference
     let now =
         new Date(document.getElementById("cdate").value);
     console.log(now);
@@ -20,7 +12,6 @@ function getDOB() {
     let monthDiff = now.getMonth() - month;
     let dateDiff = now.getDate() - day;
 
-    // Calculating the Age
     if (yearDiff < 0) console.log("invalid date");
     else if (monthDiff > 0) {
         console.log(yearDiff);
@@ -36,9 +27,6 @@ function getDOB() {
         dateDiff = 30 + dateDiff;
         monthDiff -= 1;
     }
-
-    // Show calculated age as output
-    // and invalid if wrong input is given
     if (yearDiff < 0)
         document.getElementById("currentAge").innerHTML = "Invalid Date";
     else
@@ -47,7 +35,6 @@ function getDOB() {
             + monthDiff + " months " + dateDiff + " days";
 }
 
-// Function to provide default date value
 function currentDate() {
     console.log(formatted());
     let d = document.getElementById("cdate");
@@ -64,7 +51,4 @@ function formatted(date = new Date()) {
 function short(num) {
     return num.toString().padStart(2, "0");
 }
-
-// Calling current date function
-// to set default date value
 currentDate();
